bd35223f90bd7f239929faf628333f1bcff7c2f0
sedigheh <sd.farasat@gmail.com>
2024-02-17 20:15:40 +0330

    Add double moving averages cross over strategy

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
 import market_data
-from strategies.moving_average_cross_over import ma_price_cross_over
+import sys
+from strategies.moving_average_cross_over import ma_price_cross_over, double_ma_cross_over
 
 
 if __name__ == '__main__':
-    ins_code = "25244329144808274"
-    day = 200
-    sma_period = 20
+    ins_code = "17914401175772326"
+    day = 750
+    # sma_period = 20
+    fast_period = 20
+    slow_period = 50
 
     # Gets data
     prices = market_data.get_closing_prices(ins_code, day)
     adj_prices = market_data.adjust_closing_prices(prices)
 
     # Strategy
-    buy_signals, sell_signals = ma_price_cross_over(adj_prices, sma_period)
+    # buy_signals, sell_signals = ma_price_cross_over(adj_prices, sma_period)
+    buy_signals, sell_signals = double_ma_cross_over(adj_prices, fast_period, slow_period)
 
     # Backtest the strategy
+    if not buy_signals or not sell_signals:
+        print('No Signal!')
+        sys.exit()
+
     if sell_signals[0] < buy_signals[0]:
         sell_signals.pop(0)
     if len(buy_signals) > len(sell_signals):
@@ -22,7 +30,7 @@ if __name__ == '__main__':
 
     PnL = []
     for i, signal in enumerate(buy_signals):
-        PnL.append(adj_prices[sma_period + sell_signals[i] - 1] - adj_prices[sma_period + signal - 1])
+        PnL.append(adj_prices[sell_signals[i] - 1] - adj_prices[signal - 1])
     profit_count = len(list(filter(lambda n: n >= 0, PnL)))
     loss_count = len(list(filter(lambda n: n < 0, PnL)))
     total_profit = sum(list(filter(lambda n: n >= 0, PnL)))
@@ -31,6 +39,6 @@ if __name__ == '__main__':
 
     print('Number of profitable trades: ', profit_count)
     print('Number of losing trades: ', loss_count)
-    print('Total value of profitable trades (in Rials): ', round(total_profit))
-    print('Total value of losing trades (in Rials): ', round(total_loss))
-    print('Total PnL (in Rials): ', round(total_PnL))
+    print(f'Total value of profitable trades: {round(total_profit):,} Rials')
+    print(f'Total value of losing trades: {round(total_loss):,} Rials')
+    print(f'Total PnL: {round(total_PnL):,} Rials')
---
+++ tests/test_moving_average_cross_over.py
@@ -0,0 +1,13 @@
+import random
+from strategies.moving_average_cross_over import double_ma_cross_over
+from unittest import TestCase
+
+
+class Test(TestCase):
+    def test_double_ma_cross_over(self):
+        adj_prices = random.sample(range(1200, 3400), 400)
+        fast_period = 20
+        slow_period = 50
+        buy_signals, sell_signals = double_ma_cross_over(adj_prices, fast_period, slow_period)
+
+        self.assertNotEqual(len(buy_signals), 0)
