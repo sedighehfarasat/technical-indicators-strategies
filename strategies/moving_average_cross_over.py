e3b41efe645c9b9d17674c1ba02b6658d4b24455
sedigheh <sd.farasat@gmail.com>
2024-02-25 13:33:23 +0330

    Add double_ma_price_cross_over strategy

--- strategies/moving_average_cross_over.py
+++ strategies/moving_average_cross_over.py
@@ -31,3 +31,22 @@ def double_ma_cross_over(adj_prices, fast_period, slow_period):
             sell_signals.append(slow_period + i)
 
     return buy_signals, sell_signals
+
+
+def double_ma_price_cross_over(adj_prices, fast_period, slow_period):
+    # Calculates simple moving averages
+    fast_sma = list(technical_indicators.simple_moving_average(adj_prices, fast_period))
+    slow_sma = list(technical_indicators.simple_moving_average(adj_prices, slow_period))
+    del fast_sma[0: slow_period - fast_period]
+    # Generates buy/sell signals
+    buy_signals = []
+    sell_signals = []
+    for i in range(len(slow_sma)):
+        if (adj_prices[slow_period + i - 1] >= fast_sma[i] >= slow_sma[i]
+                and adj_prices[slow_period + i - 1 - 1] < fast_sma[i - 1] < slow_sma[i - 1]):
+            buy_signals.append(slow_period + i)
+        elif (adj_prices[slow_period + i - 1] < fast_sma[i] < slow_sma[i]
+              and adj_prices[slow_period + i - 1 - 1] >= fast_sma[i - 1] >= slow_sma[i - 1]):
+            sell_signals.append(slow_period + i)
+
+    return buy_signals, sell_signals
--- tests/test_moving_average_cross_over.py
+++ tests/test_moving_average_cross_over.py
@@ -1,5 +1,5 @@
 import random
-from strategies.moving_average_cross_over import double_ma_cross_over
+from strategies.moving_average_cross_over import double_ma_cross_over, double_ma_price_cross_over
 from unittest import TestCase
 
 
@@ -9,5 +9,13 @@ class Test(TestCase):
         fast_period = 20
         slow_period = 50
         buy_signals, sell_signals = double_ma_cross_over(adj_prices, fast_period, slow_period)
+
+        self.assertNotEqual(len(buy_signals), 0)
+
+    def test_double_ma_price_cross_over(self):
+        adj_prices = random.sample(range(1200, 3400), 400)
+        fast_period = 20
+        slow_period = 50
+        buy_signals, sell_signals = double_ma_price_cross_over(adj_prices, fast_period, slow_period)
 
         self.assertNotEqual(len(buy_signals), 0)
