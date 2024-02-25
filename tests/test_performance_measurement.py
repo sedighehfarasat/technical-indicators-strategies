da81c1ce1999cfff3acd44de9572aa193d87f6dd
sedigheh <sd.farasat@gmail.com>
2024-02-25 16:12:56 +0330

    Add risk management

--- tests/test_performance_measurement.py
+++ tests/test_performance_measurement.py
@@ -1,5 +1,7 @@
+import numpy as np
 import random
 from performance_and_risk_management.performance_measurement import annualized_sharpe, annualized_sortino
+from performance_and_risk_management.risk_management import var_cov_var
 from unittest import TestCase
 
 
@@ -36,3 +38,16 @@ class PerformanceTest(TestCase):
         calmar = annualized_sortino(excess_returns, 252)
 
         self.assertNotEqual(calmar, 0)
+
+    def test_var_cov_var(self):
+        prices = random.sample(range(1200, 3400), 252)
+        returns = []
+        for i in range(len(prices) - 1):
+            returns.append((prices[i + 1] - prices[i]) / prices[i])
+        principal = 100_000_000
+        confidence_level = 0.99
+        mu = np.mean(returns)
+        sigma = np.std(returns)
+        var = var_cov_var(principal, confidence_level, mu, sigma)
+
+        self.assertNotEqual(var, 0)
